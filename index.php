1368d71b7b4f07941a5f47edd31f5cd8df2f76cb
kamildziuk <kontakt@kamildziuk.pl>
2024-12-13 14:09:28 +0100

    Improving responsiveness v3 (styles)

--- index.php
+++ index.php
@@ -19,13 +19,130 @@
     <h2 class="textHead">Optivai</h2>
     <pre class="language-css" tabindex="0">
     <code class="language-css"><span class="token property">Test1</span><span class="token property">Test2</span><span class="token property">Test3</span></code></pre>
-  
-   
-
-
-
-
     </section>
+
+    
+    <div class="container">
+      <div class="image">
+        <img src="optivAI1.jpg">
+      </div>
+      <div class="text">
+        <h1>Text example, text example, text example, text example, text example, text example, text example </h1>
+      </div>
+    </div>
+
+    
+    <div class="container1">
+ 
+      <div class="text1">
+        <h1>Text example, text example, text example, text example, text example, text example, text example </h1>
+      </div>
+      <div class="image1">
+        <img src="optivAI1.jpg">
+      </div>
+    </div>
+
+    <style>
+  .container {
+position: absolute;
+padding-top: 100px !important ;
+    margin-top: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: center
+}
+
+img {
+    position: relative;
+  max-width: 70%;
+  max-height:70%;
+border-radius: 2%;
+padding-left:10%;
+}
+
+.text {
+
+  
+    padding-right:10%;
+  font-size:17px;
+
+}
+
+
+
+.container1 {
+position: absolute;
+padding-top: 100px !important ;
+    margin-top: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center
+}
+
+
+
+.text1 {
+
+  
+    padding-left:10%;
+  font-size:17px;
+
+}
+
+@media screen and (max-width:800px) {
+    .container {
+display: block;
+
+}
+img {
+    position: relative;
+  max-width: 70%;
+  max-height:70%;
+border-radius: 2%;
+margin-bottom: 2%;
+padding-left:0%;
+transform: translateX(-50%);
+left: 50%;
+}
+
+.text {
+
+  
+padding-right:0%;
+font-size:13px;
+
+}
+
+
+
+.container1 {
+display: block;
+
+}
+
+
+.text1 {
+
+    margin-top: 70%;
+padding-left:0% !important;
+font-size:13px;
+
+}
+}
+@media screen and (max-width:300px) {
+
+
+    .text1 {
+
+margin-top: 90%;
+padding-left:0% !important;
+font-size:13px;
+
+}
+
+    
+}
+  </style>
     <!-- head section end-->
 
     <script src="js/script.js" defer></script>
